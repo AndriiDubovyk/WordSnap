ca4a7d8d78ca8d53d41d35d78ed52e6f0d88f4ae
Andrii Dubovyk <and.dubovyk@gmail.com>
2023-04-21 18:32:49 +0300

    Change study screen UI

--- app/src/main/java/com/andriidubovyk/wordsnap/presentation/screens/study/StudyScreen.kt
+++ app/src/main/java/com/andriidubovyk/wordsnap/presentation/screens/study/StudyScreen.kt
@@ -4,10 +4,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.ExtendedFloatingActionButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -19,30 +16,18 @@ import com.andriidubovyk.wordsnap.presentation.navigation.Screen
 
 @Composable
 fun StudyScreen(navController: NavController) {
-    Scaffold(
-        floatingActionButton = {
-            ExtendedFloatingActionButton(
-                onClick = {
-                    navController.navigate(Screen.PracticeFlashcard.route)
-                },
-                containerColor = MaterialTheme.colorScheme.primary,
-                shape = CircleShape,
-            ) {
-                Text(
-                    text = stringResource(R.string.start),
-                    style = MaterialTheme.typography.headlineMedium
-                )
-            }
-        }
+    Box(
+        modifier = Modifier
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
     ) {
-        Box(
-            modifier = Modifier
-                .padding(it)
-                .fillMaxSize(),
-            contentAlignment = Alignment.Center
+        Button(
+            onClick = {
+                navController.navigate(Screen.PracticeFlashcard.route)
+            }
         ) {
             Text(
-                text = stringResource(R.string.ready_to_start),
+                text = stringResource(R.string.start),
                 style = MaterialTheme.typography.headlineLarge
             )
         }
