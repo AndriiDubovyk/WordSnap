b463e60bb557bceba626a77d8e1625ae40d92e6f
Andrii Dubovyk <and.dubovyk@gmail.com>
2023-04-28 15:42:06 +0300

    Prepare app for publishing

--- app/src/main/java/com/andriidubovyk/wordsnap/MainActivity.kt
+++ app/src/main/java/com/andriidubovyk/wordsnap/MainActivity.kt
@@ -13,7 +13,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.core.splashscreen.SplashScreen
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.rememberNavController
 import com.andriidubovyk.wordsnap.common.LoaderHelper
